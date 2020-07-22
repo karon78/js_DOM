ecc193c1927e4e45686996fc2c82b578aed1ef2e
Vasy Terkin <vasy.terkin12@gmail.com>
2020-07-22 05:47:41 +0300

    Add code example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+import DataLoader from './DataLoader.js';
+
+const dataLoader = new DataLoader();
+
+dataLoader.addEventListener('load', ({ detail: data }) => {
+  console.table(data);
+});
+dataLoader.addEventListener('error',
+  ({ detail: error }) => { console.error(error); });
+
+dataLoader.loadData('./persons.json'); // correct url
+dataLoader.loadData('./wrong-path.json'); // incorrect url
